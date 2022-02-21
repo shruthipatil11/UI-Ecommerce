a066ae24ddd84b9e773d61b5be02ea6ff6eaa6c0
shruthipatil11 <shrutipatil1121@gmail.com>
2022-02-21 12:25:52 +0000

    refactor: well formatted and removed unwanted comments

--- src/app/auth/auth.js
+++ src/app/auth/auth.js
@@ -3,18 +3,16 @@ const signInBtn = document.getElementById("sign-in-btn");
 const signUpForm = document.getElementById("sign-up");
 const signInForm = document.getElementById("sign-in");
 
+signUpBtn.addEventListener("click", () => {
+  signUpForm.classList.remove("display-none");
+  signInForm.classList.add("display-none");
+  signUpBtn.classList.add("display-none");
+  signInBtn.classList.remove("display-none");
+});
 
-signUpBtn.addEventListener("click", ()=>{
-    signUpForm.classList.remove("display-none");
-    signInForm.classList.add("display-none");
-    signUpBtn.classList.add("display-none");
-    signInBtn.classList.remove("display-none");
-})
-
-signInBtn.addEventListener("click", ()=>{
-    signUpBtn.classList.remove("display-none");
-    signInBtn.classList.add("display-none");
-    signUpForm.classList.add("display-none");
-    signInForm.classList.remove("display-none");
-    
-})
+signInBtn.addEventListener("click", () => {
+  signUpBtn.classList.remove("display-none");
+  signInBtn.classList.add("display-none");
+  signUpForm.classList.add("display-none");
+  signInForm.classList.remove("display-none");
+});
